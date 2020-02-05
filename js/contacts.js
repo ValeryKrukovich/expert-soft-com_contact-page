8de0613b4f0cb4492fbf29994a720598cc264b9b
ValeryKrukovich <krukovich-valery@yandex.ru>
2020-02-05 19:19:27 +0300

    add validation

--- js/contacts.js
+++ js/contacts.js
@@ -1,6 +1,29 @@
-const btnSendMessage = document.querySelector('.footer__send-message');
+const form = document.querySelector('.callback-form');
 const inputFirstName = document.querySelector('#first-name');
+const inputEmail = document.querySelector('#email');
+const fields = document.querySelectorAll('.form-group__input');
+const btnSendMessage = document.querySelector('.footer__send-message');
 
 btnSendMessage.addEventListener('click', function() {
     inputFirstName.focus();
+});
+
+for (let i = 0; i < fields.length; i++) {
+    fields[i].addEventListener("input", function (event) {
+        fields[i].classList.remove('form-group__input_error');
+        fields[i].nextElementSibling.innerHTML = '';
+    });
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    for (let i = 0; i < fields.length; i++) {
+        if (!fields[i].value) {
+            fields[i].classList.add('form-group__input_error');
+            fields[i].nextElementSibling.innerHTML = 'Cannot be blank.';
+        }
+        if (inputEmail.value !== 'vasilinakrukovich@gmail.com') {
+            inputEmail.nextElementSibling.innerHTML = 'Email is incorrect!';
+        }
+    }
 });
