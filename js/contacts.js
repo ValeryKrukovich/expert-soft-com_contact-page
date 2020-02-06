b75f3421bac7a36f41adf1794b92c76a4dd077df
ValeryKrukovich <krukovich-valery@yandex.ru>
2020-02-06 11:59:31 +0300

    fix validate

--- js/contacts.js
+++ js/contacts.js
@@ -1,19 +1,19 @@
 const form = document.querySelector('.callback-form');
-const inputFirstName = document.querySelector('#first-name');
-const inputEmail = document.querySelector('#email');
+const inputFirstName = document.querySelector('.js-first-name');
+const inputEmail = document.querySelector('.js-email');
 const fields = document.querySelectorAll('.form-group__input');
 const btnSendMessage = document.querySelector('.footer__send-message');
-
-function emailIsValid(email) {
-    return /\S+@\S+\.\S+/.test(email)
-  }
 
 btnSendMessage.addEventListener('click', function() {
     inputFirstName.focus();
 });
 
+function emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email)
+}
+
 for (let i = 0; i < fields.length; i++) {
-    fields[i].addEventListener("input", function (event) {
+    fields[i].addEventListener('input', function (event) {
         fields[i].classList.remove('form-group__input_error');
         fields[i].nextElementSibling.innerHTML = '';
     });
@@ -21,6 +21,7 @@ for (let i = 0; i < fields.length; i++) {
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
+    
     for (let i = 0; i < fields.length; i++) {
         if (!fields[i].value) {
             fields[i].classList.add('form-group__input_error');
