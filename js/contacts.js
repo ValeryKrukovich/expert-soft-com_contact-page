a8c000be357771a896e9f09ddf542816e649bbdf
ValeryKrukovich <krukovich-valery@yandex.ru>
2020-02-06 16:53:31 +0300

    fix send-message btn position

--- js/contacts.js
+++ js/contacts.js
@@ -1,13 +1,15 @@
 const form = document.querySelector('.callback-form');
-const inputFirstName = document.querySelector('.js-first-name');
-const inputEmail = document.querySelector('.js-email');
 const fields = document.querySelectorAll('.form-group__input');
+const inputFirstName = document.querySelector('#first-name');
+const inputEmail = document.querySelector('#email');
 const btnSendMessage = document.querySelector('.footer__send-message');
 
+//Focus onclick
 btnSendMessage.addEventListener('click', function() {
     inputFirstName.focus();
 });
 
+//Form Validation
 function emailIsValid(email) {
     return /\S+@\S+\.\S+/.test(email)
 }
